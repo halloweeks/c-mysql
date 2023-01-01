555552105dcd85c0cedfc3e1bc7631a49a9dfae7
Hallo Weeks <96823158+halloweeks@users.noreply.github.com>
2023-01-01 14:20:42 +0530

    Update mysql.c

--- mysql.c
+++ mysql.c
@@ -1,36 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <mariadb/mysql.h>
+#include <mysql/mysql.h>
 
 int main() {
-  MYSQL *connection;
-  MYSQL_RES *results;
-  MYSQL_ROW row;
-  
-  connection = mysql_init(NULL);
-  
-  if (!mysql_real_connect(connection, "127.0.0.1", "root", "1234", "test", 0, NULL, 0)) {
-    fprintf(stderr, "%s\n", mysql_error(connection));
-    exit(EXIT_FAILURE);
-  }
-  
-  if (mysql_query(connection, "show tables")) {
-    //fprintf(stderr, "%s\n", mysql_error(connection));
-    exit(EXIT_FAILURE);
-  }
-  
-  results = mysql_use_result(connection);
-  // int num = mysql_num_rows(results);
-  
-  // output table name
-  printf("MySQL Tables in mysql database:\n");
-  while ((row = mysql_fetch_row(results)) != NULL) {
-    printf("%s \n", row[0]);
-  }
-  
-  // close connection
-  mysql_free_result(results);
-  mysql_close(connection);
-  
-  exit(EXIT_SUCCESS);
+	MYSQL *connection;
+	MYSQL_RES *results;
+	MYSQL_ROW row;
+	
+	connection = mysql_init(NULL);
+	
+	// hostname, username, password, database name
+	if (!mysql_real_connect(connection, "127.0.0.1", "root", NULL, NULL, 0, NULL, 0)) {
+		fprintf(stderr, "%s\n", mysql_error(connection));
+		exit(EXIT_FAILURE);
+	}
+	
+	if (mysql_query(connection, "SHOW DATABASES")) {
+		fprintf(stderr, "%s\n", mysql_error(connection));
+		exit(EXIT_FAILURE);
+	}
+	
+	results = mysql_use_result(connection);
+	// int num = mysql_num_rows(results);
+	
+	printf("MySQL Databases:\n");
+	while ((row = mysql_fetch_row(results)) != NULL) {
+		printf("[%s]\n", row[0]);
+	}
+	
+	// close connection
+	mysql_free_result(results);
+	mysql_close(connection);
+	exit(EXIT_SUCCESS);
 }
